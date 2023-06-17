9ea284ab975af2e3d8c6309debbd4cb600490794
ACGreve <greve5921@gmail.com>
2023-06-16 22:46:05 -0400

    Added font type, box for drops, and fixed screen resizing issue.

--- script.js
+++ script.js
@@ -54,3 +54,29 @@ setInterval(generateImage, 500); // Adjust the interval as needed
 
 // Start the animation
 animate();
+
+
+Notes for Image location randomization:
+https://replit.com/talk/ask/JS-place-image-on-random-coordinates/48068
+
+
+function randomizeImg(el) {
+  el.style.position = "absolute";
+  el.style.top = "50px";
+  el.style.left = "50px";
+}
+let element = document.getElementById("something");
+randomizeImg(element);
+
+
+
+function randint(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+function randomizeImg(el) {
+  el.style.position = "absolute";
+  el.style.top = randint(0, 100) + "px";
+  el.style.left = randint(0, 100) + "px";
+}
+let element = document.getElementById("something");
+randomizeImg(element);
